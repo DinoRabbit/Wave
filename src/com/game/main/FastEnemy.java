c213502d73c9d67f901483f87033053ad0019249
DinoRabbit <36932658+DinoRabbit@users.noreply.github.com>
2018-05-20 10:42:18 -0400

    Update FastEnemy.java
    Added functionality to control initial direction.

--- src/com/game/main/FastEnemy.java
+++ src/com/game/main/FastEnemy.java
@@ -11,7 +11,7 @@ public class FastEnemy extends GameObject
 	
 	private Handler handler;
 
-	public FastEnemy(int x, int y, ID id, boolean horiz, Handler handler) 
+	public FastEnemy(int x, int y, int mode, ID id, boolean horiz, Handler handler) 
 	{
 		super(x, y, id);
 		this.handler = handler;
@@ -19,13 +19,53 @@ public class FastEnemy extends GameObject
 		//Much faster vertical or horizontal movement
 		if(horiz)
 		{
-			velX = 9;
-			velY = 2;
+			switch(mode)
+			{
+				case 1:
+					velX = 9;
+					velY = 2;
+					break;
+				case 2:
+					velX = -9;
+					velY = 2;
+					break;
+				case 3:
+					velX = 9;
+					velY = -2;
+					break;
+				case 4:
+					velX = -9;
+					velY = -2;
+					break;
+				default:
+					velX = 9;
+					velY = 2;
+			}
 		}
 		else
 		{
-			velX = 2;
-			velY = 9;
+			switch(mode)
+			{
+				case 1:
+					velX = 2;
+					velY = 9;
+					break;
+				case 2:
+					velX = -2;
+					velY = 9;
+					break;
+				case 3:
+					velX = 2;
+					velY = -9;
+					break;
+				case 4:
+					velX = -2;
+					velY = -9;
+					break;
+				default:
+					velX = 2;
+					velY = 9;
+			}
 		}
 		
 	}
